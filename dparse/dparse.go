90b5ae413d5c5db2201f43ed2c1b7da4934dd04e
Salvador Fuentes Jr <fuentesjr@gmail.com>
2011-10-03 22:36:52 -0700

    write out csv output data

--- dparse/dparse.go
+++ dparse/dparse.go
@@ -1,42 +1,83 @@
 package main
-import "time"
+//import "time"
 import "fmt"
 import "csv"
 import "os"
+import "regexp"
 
-func exit_if_done(err os.Error) {
-  if err != nil {
-    if err == os.EOF {
-      fmt.Printf("EOF.\n")
-      os.Exit(0)
-    } else {
-      fmt.Printf("Received error... terminating.\n")
-      os.Exit(1)
-    }
+func exit_on_errors(err os.Error) {
+  if err != nil && err != os.EOF {
+    //log.Fatal(err)
+    fmt.Printf("Received error: %s ... terminating with code 1.\n", err)
+    os.Exit(1)
   }
 }
 
+func get_layout(text string) (string, os.Error) {
+  // Map of format layouts to regular expressions
+  regexps := make(map[string]*regexp.Regexp)
+  regexps["01/02/2006 03:04"], _ = regexp.Compile("^[0-9][0-9]/[0-9][0-9]/([0-9][0-9]|[0-9][0-9][0-9][0-9])")
+  regexps["01-02-2006 03:04"], _ = regexp.Compile("^[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]")
+  regexps["01-02-06 03:04"], _ = regexp.Compile("^[0-9][0-9]-[0-9][0-9]-[0-9][0-9]")
+
+
+  for layout, re := range regexps {
+    //matches := re.Find(bytes.NewBufferString(text).Bytes()) 
+    matches := re.FindString(text)
+    if matches != "" {
+      fmt.Println("Found match and layout ==> " + layout)
+      return layout, nil
+    }
+  }
+
+  return "", fmt.Errorf("Unrecognized pattern in: %g", text)
+}
+
 func main() {
-  filename := "./data.csv"
+  filename := "./raw_subscribers.csv"
   f, err := os.Open(filename)
   if err != nil { fmt.Println("Failed to open " + filename); os.Exit(1) }
   defer f.Close()
 
+  filename = "test.csv"
+  outf, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0644)
+  if err != nil { fmt.Println(err); os.Exit(1) }
+  defer outf.Close()
+
   c := csv.NewReader(f)
-  layout := "01/02/2006"
+  c.TrailingComma = true
+  outc := csv.NewWriter(outf)
+  defer outc.Flush()
 
   // Grab CSV headers
-  headers, err := c.Read()
-  exit_if_done(err)
+  _, err = c.Read()
+  exit_on_errors(err)
 
-  //for row, err := c.Read() && err != os.EOF {
-  for row, err := c.Read();; row, err = c.Read() {
-    exit_if_done(err)
-    t, _ := time.Parse(layout, row[0])
-    fmt.Println(t)
+  for {
+    row, err := c.Read()
+    if err != nil {
+      if err != os.EOF {
+        fmt.Println(err)
+        os.Exit(1)
+      } else {
+        break;
+      }
+    }
 
+    //layout, err := get_layout(row[0])
+    //fmt.Println("Using layout ==> " + layout)
+    //t, _ := time.Parse(layout, row[0])
+    //fmt.Println(t)
+
+    /*
     for i, field := range row {
       fmt.Printf(headers[i] + " => " + field + "\n")
     }
+    */
+    if err := outc.Write(row); err != nil {
+      fmt.Println(err)
+    }
   }
+
+  fmt.Println("Done. Good bye.\n")
 }
